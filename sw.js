645c7aa7f519ab547979fbd87bcce1a6a352bc22
Waldemar Reusch <lordvlad@users.noreply.github.com>
2018-10-24 08:50:36 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* globals workbox, importScripts */
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js')
 
-workbox.routing.registerRoute(/.*\.(mjs|js|css|html)/, workbox.strategies.networkFirst())
+workbox.routing.registerRoute(/.*\.(mjs|js|css|html)/, workbox.strategies.staleWhileRevalidate())
 workbox.routing.registerRoute('/', workbox.strategies.networkFirst())
