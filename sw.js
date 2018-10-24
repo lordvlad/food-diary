1621b2d258e2a8ce61c4cd17a7878ce68e9bc2e0
Waldemar Reusch <lordvlad@users.noreply.github.com>
2018-10-24 13:01:51 +0200

    cache svgs, invalidate index cache in background

--- sw.js
+++ sw.js
@@ -2,4 +2,5 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js')
 
 workbox.routing.registerRoute(/.*\.(mjs|js|css|html|woff2)/, workbox.strategies.staleWhileRevalidate())
-workbox.routing.registerRoute('/', workbox.strategies.networkFirst())
+workbox.routing.registerRoute(/.*\.svg.*/, workbox.strategies.cacheFirst())
+workbox.routing.registerRoute('/', workbox.strategies.staleWhileRevalidate())
