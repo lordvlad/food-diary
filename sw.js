babd80f6be3551b508b888cce8e02cd0d00daa38
Waldemar Reusch <waldemar.reusch@googlemail.com>
2018-10-24 22:24:48 +0200

    don't cache woff files

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* globals workbox, importScripts */
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js')
 
-workbox.routing.registerRoute(/.*\.(mjs|js|css|html|woff2).*/, workbox.strategies.staleWhileRevalidate())
+workbox.routing.registerRoute(/.*\.(mjs|js|css|html).*/, workbox.strategies.staleWhileRevalidate())
 workbox.routing.registerRoute(/.*\.svg.*/, workbox.strategies.cacheFirst())
 workbox.routing.registerRoute('/', workbox.strategies.staleWhileRevalidate())
