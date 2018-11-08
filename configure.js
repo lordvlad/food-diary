be396009e23c16c434ba086f5acb19b352e675fd
Waldemar Reusch <waldemar.reusch@googlemail.com>
2018-11-08 23:13:18 +0100

    fix formatting

--- configure.js
+++ configure.js
@@ -5,11 +5,9 @@ const { createInterface } = require('readline')
 const rl = createInterface({input: process.stdin, output: process.stdout})
 
 rl.question('Enter GCM API Key: ', gcmApiKey => {
-    const keys = generateVAPIDKeys();
-    appendFileSync('.gcm_api_key',  gcmApiKey)
-    appendFileSync('.vapid_public_key', keys.publicKey)
-    appendFileSync('.vapid_private_key', keys.privateKey)
-    rl.close()
+  const keys = generateVAPIDKeys()
+  appendFileSync('.gcm_api_key', gcmApiKey)
+  appendFileSync('.vapid_public_key', keys.publicKey)
+  appendFileSync('.vapid_private_key', keys.privateKey)
+  rl.close()
 })
-
-
