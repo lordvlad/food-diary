9185970c0eb8831c4b976840890d476b90064b3a
Waldemar Reusch <lordvlad@users.noreply.github.com>
2018-11-28 13:22:17 +0100

    reduce output

--- api/store.js
+++ api/store.js
@@ -3,13 +3,11 @@ const { stringify } = JSON
 const storeUrl = process.env.KEYVALUE_XYZ_URL
 
 const load = async _ => {
-  console.log(`loading from ${storeUrl}`)
   const response = await fetch(storeUrl)
   try { return await response.json() } catch (e) { return [] }
 }
 
 const store = async subscriptions => {
-  console.log(`storing ${subscriptions.length} subscriptions to ${storeUrl}`)
   const body = stringify(subscriptions)
   const response = await fetch(storeUrl, { method: 'POST', body })
   if (!response.ok) throw new Error(response.statusText)
