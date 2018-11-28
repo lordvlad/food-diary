1562398be4ba2659bdf67345ccdd4eb2e9d6c14a
Waldemar Reusch <lordvlad@users.noreply.github.com>
2018-11-28 13:44:53 +0100

    Update notify.js

--- api/notify.js
+++ api/notify.js
@@ -1,15 +1,15 @@
 const { send } = require('micro')
+const { parse } = require('url')
 const fetch = require('node-fetch').default
 const webpush = require('@lordvlad/web-push')
 const { load } = require('./store')
 const { stringify } = JSON
-const { GCM_API_KEY, VAPID_PUBLIC_KEY, VAPID_PRIVATE_KEY, NOW_URL } = process.env
+const { GCM_API_KEY, VAPID_PUBLIC_KEY, VAPID_PRIVATE_KEY } = process.env
 
 webpush.setVapidDetails('http://food-diary.now.sh', VAPID_PUBLIC_KEY, VAPID_PRIVATE_KEY)
 webpush.setGCMAPIKey(GCM_API_KEY)
 
 module.exports = async (req, res) => {
-  console.log(`NOW_URL ${NOW_URL}`)
   try {
     const payload = 'wake up!'
     const subscriptions = await load()
@@ -21,7 +21,9 @@ module.exports = async (req, res) => {
         console.log(`[OK]   ${endpoint} notified`)
       } catch (e) {
         console.error(`[FAIL] ${endpoint} not notified: ${e.message}`)
-        await fetch(`${NOW_URL}/api/unsubscribe`, { method: 'POST', body: stringify({ endpoint }) })
+        const u = parse(req.url)
+        console.log(`unsubscribing ${u.}/api/unsubscribe`)
+        await fetch(`${u.protocol}://${u.host}/api/unsubscribe`, { method: 'POST', body: stringify({ endpoint }) })
       }
     }
     send(res, 200)
