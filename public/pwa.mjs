4045aed938fbe679a05405f0b3d7077422afe4c0
Waldemar Reusch <waldemar.reusch@googlemail.com>
2018-12-02 19:33:39 +0100

    more consistent code

--- public/pwa.mjs
+++ public/pwa.mjs
@@ -11,10 +11,11 @@ export const store = async (_, emitter) => {
   const { emit } = autobind(emitter)
   const registration = await serviceWorker.register('./sw.js')
   const click = e => { e.preventDefault(); emit(reload) }
+  const onmessage = e => emit(swMessage, e.data)
   const updated = _ => emit('toast', html`
     Application updated. <a href=# onclick=${click}>Click here</a> to reload.`)
   const statechange = _ => { if (registration.installing.state === 'activated') updated() }
   const onupdatefound = _ => registration.installing.onstatechange(statechange)
-  serviceWorker.addEventListener(message, e => emit(swMessage, e.data))
+  serviceWorker.addEventListener(message, onmessage)
   registration.addEventListener(updatefound, onupdatefound)
 }
