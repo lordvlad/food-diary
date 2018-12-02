695ecab5bd05215945ff50b86e2749f637d76c93
Waldemar Reusch <waldemar.reusch@googlemail.com>
2018-12-02 13:40:27 +0100

    add space before 'not'

--- api/checkSubscription.js
+++ api/checkSubscription.js
@@ -6,7 +6,7 @@ module.exports = async (req, res) => {
     const { endpoint } = await json(req)
     const test = s => s.endpoint === endpoint
     const contains = (await load()).some(test)
-    console.log(`[OK]   ${endpoint}${contains ? '' : 'not'} known`)
+    console.log(`[OK]   ${endpoint}${contains ? '' : ' not'} known`)
     send(res, contains ? 200 : 404)
   } catch (e) {
     console.error(`[FAIL] ${e.message}`)
