5c0f9a18c3773219727558c18316d9f7f38f2658
Waldemar Reusch <waldemar.reusch@googlemail.com>
2018-12-02 19:51:08 +0100

    better handle sw updates

--- public/pwa.mjs
+++ public/pwa.mjs
@@ -10,12 +10,24 @@ export const events = { swMessage }
 export const store = async (_, emitter) => {
   const { emit } = autobind(emitter)
   const registration = await serviceWorker.register('./sw.js')
-  const click = e => { e.preventDefault(); emit(reload) }
-  const onmessage = e => emit(swMessage, e.data)
-  const updated = _ => emit('toast', html`
-    Application updated. <a href=# onclick=${click}>Click here</a> to reload.`)
-  const statechange = _ => { if (registration.installing.state === 'activated') updated() }
-  const onupdatefound = _ => registration.installing.onstatechange(statechange)
-  serviceWorker.addEventListener(message, onmessage)
-  registration.addEventListener(updatefound, onupdatefound)
+  const updated = _ => {
+    const click = e => { e.preventDefault(); emit(reload) }
+    emit('toast', html`Application updated.
+      <a href=# onclick=${click}>Click here</a> to reload.`)
+  }
+  serviceWorker.addEventListener(message, e => emit(swMessage, e.data))
+  registration.addEventListener(updatefound, _ => {
+    if (!registration.installing && registration.active) {
+      // new worker already activated
+      updated()
+    } else if (registration.installing) {
+      // new worker in installation phase
+      registration.installing.addEventListener('statechange', _ => {
+        if (registration.installing.state === 'activated') updated()
+      })
+    } else {
+      console.error('ran into unhandled state')
+      console.dir(registration)
+    }
+  })
 }
